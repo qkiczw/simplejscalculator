761cdd7ce8a7fa855de847df67c6096cabdf1f12
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2019-02-06 22:12:08 +0100

    Now pressed button is showing on the calc screen

--- script/calculator.js
+++ script/calculator.js
@@ -1,2 +1,22 @@
 // Calculator script
-console.log('Hello !!!')
+console.log('Hello !!!');
+
+let numBtns = document.querySelectorAll('.calc-btn-num');
+let actionBtns = document.querySelectorAll('.calc-btn-action');
+const calcScreen = document.querySelector('.result');
+let displayNum = '0';
+
+calcScreen.innerHTML = displayNum;
+
+function displayMessage(e) {
+    let pushedNum = e.target.innerHTML;
+    displayNum += pushedNum;
+    if ( displayNum.charAt(0) === '0') {
+        displayNum =  displayNum.slice(1);
+        console.log(displayNum);    
+    };
+    calcScreen.innerHTML = displayNum;
+    
+}
+
+numBtns.forEach( num => num.addEventListener('click', displayMessage));
