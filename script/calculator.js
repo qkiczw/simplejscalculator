4906e971dfdcd8fcad7276ecb628d85ee49cb18e
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2019-02-13 19:00:27 +0100

    Sum button works

--- script/calculator.js
+++ script/calculator.js
@@ -3,8 +3,10 @@ console.log('Hello !!!');
 
 let numBtns = document.querySelectorAll('.calc-btn-num');
 let actionBtns = document.querySelectorAll('.calc-btn-action');
+let sumBtn = document.querySelector('.calc-btn-sum');
 const calcScreen = document.querySelector('.result');
 let displayNum = '0';
+let valusArray = [];
 
 calcScreen.innerHTML = displayNum;
 
@@ -19,4 +21,32 @@ function displayMessage(e) {
     
 }
 
+function makeOperation(e) {
+    let pushedOperation = e.target.innerHTML;
+    console.log(pushedOperation);
+    if (pushedOperation === '+') {
+        valusArray.push(displayNum);
+        valusArray.push(pushedOperation);
+        console.log(valusArray);
+        displayNum = '0'
+    }
+    if (pushedOperation === '-') {
+        valusArray.push(displayNum);
+        valusArray.push(pushedOperation);
+        console.log(valusArray);
+        displayNum = '0'
+    }
+}
+
+function makeSum(e) {
+    let pushedOperation = e.target.innerHTML;
+    valusArray.push(displayNum);
+    console.log(valusArray);
+    valString = valusArray.join(" ");
+    console.log(valString);
+    calcScreen.innerHTML = eval(valString);
+}
+
 numBtns.forEach( num => num.addEventListener('click', displayMessage));
+actionBtns.forEach( action => action.addEventListener('click', makeOperation));
+sumBtn.addEventListener('click', makeSum);
